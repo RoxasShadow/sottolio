2452a88cde00cbef819cdee76a242908da4bc3f8
Giovanni Capuano <webmaster@giovannicapuano.net>
2013-12-09 16:46:41 +0100

    Add the license and save locally the js libraries

--- app/utils.rb
+++ app/utils.rb
@@ -1,23 +1,21 @@
-class Array
-  def to_js
-    [].tap { |s| each { |a| s << "#{a}, " } }.join[0..-3]
-  end
-
-  def to_s
-    self.inspect
-  end
-end
-
-class Hash
-  def to_js
-    ['{ '].tap { |s| keys.each_with_index { |k, i| s << "#{keys[i]}: '#{values[i]}', " } }.join[0..-3] + ' }'
-  end
-
-  def to_s
-    self.inspect
-  end
-end
-
+#--
+# Copyright(C) 2013 Giovanni Capuano <webmaster@giovannicapuano.net>
+#
+# This file is part of sottolio.
+#
+# Botémon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Botémon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Botémon.  If not, see <http://www.gnu.org/licenses/>.
+#++
 class String
   def apply(database, pattern = /\#(.+)\#/)
     r = ''
