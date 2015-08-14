8bd3da2d670a1f174c76ea6f5aa61fe080f101fa
RoxasShadow <webmaster@giovannicapuano.net>
2015-08-14 14:45:19 +0200

    Refactor case statement in the main loop
    Also fixed the `Uncaught Error: unexpected return` warning on webkit

--- opal/sottolio/bootstrapper.rb
+++ opal/sottolio/bootstrapper.rb
@@ -54,33 +54,33 @@ module Sottolio
           current_command = script.keys.first
           case current_command
           when :play_sound
-            return next_dialogue.call unless script[:play_sound].true? database
-
-            sound = Sound.new(script[:play_sound][:resource], script[:play_sound][:loop], script[:play_sound][:volume])
-            sound_manager.add script[:play_sound][:id], sound
-            sound_manager.play script[:play_sound][:id]
+            if script[:play_sound].true? database
+              sound = Sound.new script[:play_sound][:resource], script[:play_sound][:loop], script[:play_sound][:volume]
+              sound_manager.add script[:play_sound][:id], sound
+              sound_manager.play script[:play_sound][:id]
+            end
 
             next_dialogue.call
           when :stop_sound
-            return next_dialogue.call unless script[:stop_sound].true? database
-
-            sound_manager.stop script[:stop_sound][:id]
+            if script[:stop_sound].true? database
+              sound_manager.stop script[:stop_sound][:id]
+            end
 
             next_dialogue.call
           when :background
-            return next_dialogue.call unless script[:background].true? database
-
-            background = Background.new(canvas_id, script[:background][:resource], script[:background][:id])
-            image_manager.add background
-            image_manager.draw script[:background][:id]
+            if script[:background].true? database
+              background = Background.new canvas_id, script[:background][:resource], script[:background][:id]
+              image_manager.add background
+              image_manager.draw script[:background][:id]
+            end
 
             next_dialogue.call
           when :character
-            return next_dialogue.call unless script[:character].true? database
-
-            character = Character.new(canvas_id, script[:character][:resource], script[:character][:id])
-            image_manager.add character
-            image_manager.draw script[:character][:id], script[:character][:x], script[:character][:y]
+            if script[:character].true? database
+              character = Character.new canvas_id, script[:character][:resource], script[:character][:id]
+              image_manager.add character
+              image_manager.draw script[:character][:id], script[:character][:x], script[:character][:y]
+            end
 
             next_dialogue.call
           when :remove
@@ -88,39 +88,45 @@ module Sottolio
 
             next_dialogue.call
           when :dialogue
-            return next_dialogue.call unless script[:dialogue].true? database
+            if script[:dialogue].true? database
+              canvas.clear
 
-            canvas.clear
-
-            if script[:dialogue].include? :name
-              canvas_text.write "#{script[:dialogue][:name].apply(database)}: #{script[:dialogue][:text].apply(database)}"
+              if script[:dialogue].include? :name
+                canvas_text.write "#{script[:dialogue][:name].apply(database)}: #{script[:dialogue][:text].apply(database)}"
+              else
+                canvas_text.write script[:dialogue][:text].apply(database)
+              end
             else
-              canvas_text.write script[:dialogue][:text].apply(database)
+              next_dialogue.call
             end
           when :input
-            return next_dialogue.call unless script[:input].true? database
+            if script[:input].true? database
+              canvas.clear
 
-            canvas.clear
+              if script[:input].include? :name
+                canvas_text.write "#{script[:input][:name].apply(database)}: #{script[:input][:text].apply(database)}"
+              else
+                canvas_text.write script[:input][:text].apply(database)
+              end
 
-            if script[:input].include? :name
-              canvas_text.write "#{script[:input][:name].apply(database)}: #{script[:input][:text].apply(database)}"
+              input = CanvasInput.new canvas_id, database, script[:input][:id], script[:input][:request].apply(database)
             else
-              canvas_text.write script[:input][:text].apply(database)
+              next_dialogue.call
             end
-
-            input = CanvasInput.new canvas_id, database, script[:input][:id], script[:input][:request].apply(database)
           when :choice
-            return next_dialogue.call unless script[:choice].true? database
+            if script[:choice].true? database
+              canvas.clear
 
-            canvas.clear
+              if script[:choice].include? :name
+                canvas_text.write "#{script[:choice][:name].apply(database)}: #{script[:choice][:text].apply(database)}"
+              else
+                canvas_text.write script[:choice][:text].apply(database)
+              end
 
-            if script[:choice].include? :name
-              canvas_text.write "#{script[:choice][:name].apply(database)}: #{script[:choice][:text].apply(database)}"
+              canvas_button.get_choice database, script[:choice][:options], script[:choice][:id], next_dialogue
             else
-              canvas_text.write script[:choice][:text].apply(database)
+              next_dialogue.call
             end
-
-            canvas_button.get_choice database, script[:choice][:options], script[:choice][:id], next_dialogue
           end
         end
       }
